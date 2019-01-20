4aa37d533aa844b532659a3e0a92e51bac1bc821
Martin Foltin <matoqq@gmail.com>
2019-01-20 15:13:06 +0100

    added Metadata
    Deleted Tutorial folder (unnecessary clutter)
    Editted site meta tags

--- server/index.php
+++ server/index.php
@@ -1,20 +1,26 @@
 <html>
     <head>
-        <title>Home</title>
+        <title>Internet Thermometer</title>
+        <meta property="og:site_name" content="Internet Thermometer">
+        <meta property="og:title" content="Internet Thermometer" />
+        <meta property="og:description" content="Arduino broadcasting temperature through Ethernet shield" />
+        <meta property="og:image" itemprop="image" content="https://cdn-learn.adafruit.com/assets/assets/000/000/476/original/temperature_tmp36fritz.gif?1447975816">
+        <meta property="og:type" content="website" />
+        <meta property="og:updated_time" content="1547980000" />
     </head>
-    
+
     <body>
         <table width="100%" border="1px solid black">
             <tr><td>
                 <a href="/database.php">Database</a>
                 <a href="/graph.php">Graph</a>
                 <!--<a href="../">Home</a>-->
-                
+
                 </td></tr>
             <tr><td><h1>Home</h1></td></tr>
         </table><br>
-        
+
         <img src="https://cdn-learn.adafruit.com/assets/assets/000/000/476/original/temperature_tmp36fritz.gif?1447975816">
-        
+
     </body>
 </html>
